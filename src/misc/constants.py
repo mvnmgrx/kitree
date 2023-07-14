220e6446d79054a00f5c8b5ff6d53d32f2c32839
Marvin Mager <99667992+mvnmgrx@users.noreply.github.com>
2023-07-14 21:18:41 +0200

    misc/constants: More constants

--- src/misc/constants.py
+++ src/misc/constants.py
@@ -1,2 +1,13 @@
-KITREE_VERSION = 'v0.0-rc1'
-KITREE_AUTHOR = '(C) Marvin Mager - @mvnmgrx - 2023'
+from os import path
+
+KITREE_VERSION = "v0.0-rc1"
+KITREE_AUTHOR = "(C) Marvin Mager - @mvnmgrx - 2023"
+KITREE_KEYWORD_FP = "Footprint"
+KITREE_KEYWORD_FP_OLD = "Footprint (old)"
+KITREE_KEYWORD_SYM = "Symbol"
+KITREE_KEYWORD_SYM_OLD = "Symbol (old)"
+KITREE_KEYWORD_3DM = "3D-Model"
+KITREE_KEYWORD_3DM_OLD = "3D-Model (old)"
+
+KITREE_CONFIG_DIR = path.join(path.expanduser('~'), '.kitree')
+"""Configuration directory base path (usually in user's home directory)"""
