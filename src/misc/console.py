a12d32c4f0b41b3f291089b25f22a041b45cf02e
Marvin Mager <99667992+mvnmgrx@users.noreply.github.com>
2023-12-05 20:02:44 +0100

    misc/console: Showing name when project is loaded

--- src/misc/console.py
+++ src/misc/console.py
@@ -8,14 +8,16 @@ License identifier:
 """
 
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Callable, Dict
+from typing import Callable, Dict
 
-from misc.colors import Color
+from misc.colors import Color as C
 from misc.logger import Logger
 
+
 @dataclass
-class Console():
+class Console:
     """This class is used to proxy console input and output to the CLI"""
+
     indentationLevel: int = 0
     commands: Dict[str, Callable] = field(default_factory=dict)
     isRunning: bool = True
@@ -28,26 +30,29 @@ class Console():
         Returns:
             - str: User input
         """
-        print('  ' * (self.indentationLevel), end='', flush=True)
-        print('> ', end='', flush=True)
+        print("  " * (self.indentationLevel), end="", flush=True)
+        if self.parent_app.project.isLoaded:
+            print(
+                f"{C.OkCyan}{self.parent_app.project.name}{C.End}> ", end="", flush=True
+            )
+        else:
+            print(f"> ", end="", flush=True)
         return input()
 
-    def write(self, message: str, color: Color = Color.End, newline = True):
+    def write(self, message: str, color: C = C.End, newline=True):
         """Writes the given message to the CLI with the given indentation level
 
         Args:
             - message (str): Message to write to the CLI
         """
-        print('  ' * (self.indentationLevel), end='', flush=True)
-        print('  ', end='', flush=True)
+        print("  " * (self.indentationLevel), end="", flush=True)
+        print("  ", end="", flush=True)
         if newline:
-            print(f'{color}{message}{Color.End}')
+            print(f"{color}{message}{C.End}")
         else:
-            print(f'{color}{message}', end='', flush=True)
+            print(f"{color}{message}", end="", flush=True)
 
-        # self.log.info(f'[ Console ] {message}')
-
-    def append(self, message: str, finish = True):
+    def append(self, message: str, finish=True):
         """Appends a message to the current line in the CLI. Used after Out(newline=False)
         to append a message to the CLI.
 
@@ -56,9 +61,9 @@ class Console():
             - finish (bool, optional): Set when the line is complete to start a newline. Defaults to True.
         """
         if finish:
-            print(f'{message}{Color.End}', flush=True)
+            print(f"{message}{C.End}", flush=True)
         else:
-            print(f'{message}', end='', flush=True)
+            print(f"{message}", end="", flush=True)
 
     def print(self, message: str):
         """Prints the given message to the CLI without indentation
@@ -80,20 +85,20 @@ class Console():
 
     def add_command(self, command: str, function: Callable):
         if command in self.commands.keys():
-            self.log.debug(f'Command {command} already in list of console commands! Skipping..')
+            self.log.debug(f"Command {command} already in list of console commands! Skipping..")
 
         self.commands.update({command: function})
         self.log.debug(f'Added "{ command }" to command list')
 
     def process_input(self, input: str):
         # Split input into its parts
-        arguments = input.split(' ')
+        arguments = input.split(" ")
         arguments = [i.strip() for i in arguments]
 
         for command in self.commands.keys():
-            if command == arguments[0]:        
+            if command == arguments[0]:
                 self.log.debug(f"Executing command '{command}' at {str(self.commands[command])}")
-                self.log.debug(f"Using arguments: {str(arguments)}")    
+                self.log.debug(f"Using arguments: {str(arguments)}")
                 self.commands[command](self.parent_app, arguments[1:])
                 break
         else:
